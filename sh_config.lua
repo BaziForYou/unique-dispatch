3cb7736f275ca8a6b7c10dae45d9f1b9b6f5c3fe
i1An <69718520+i1An@users.noreply.github.com>
2021-12-19 22:01:46 +0200

    Update sh_config.lua

--- sh_config.lua
+++ sh_config.lua
@@ -12,9 +12,7 @@ Config["CoreSettings"] = {
     ["QBCore"] = {
         ["QBCoreVersion"] = "new", -- new = using exports | old = using events
         ["QBCoreExport"] = exports['lg-core']:GetCoreObject(), -- If you are using old qbcore version just remove this line
-        ["QBUSTrigger"] = "QBCore:GetObject",
-        ["Players_Table"] = "players",
-        ["OwnedVehicles_Table"] = "player_vehicles"
+        ["QBUSTrigger"] = "QBCore:GetObject"
     },
     ["NPBase"] = {
         ["CoreName"] = "iconic-fw"
